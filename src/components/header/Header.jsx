d4109053506278987a1be5a5952c451ffb955586
AtanasG6 <gyulchev666@gmail.com>
2026-02-23 11:33:57 +0200

    Add Bootstrap Navbar with navigation links to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,22 @@
 import './header.scss'
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 export function Header() {
     return (
-        <div className="header">Header works!</div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">Task Manager</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link className='nav-link' to="/users-list">Users List</Link>
+                        <Link className='nav-link' to="/tasks-list">Tasks List</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
