9c0c7c698de2543cc94d4ff7e817529274b3f421
AtanasG6 <gyulchev666@gmail.com>
2026-02-23 08:47:21 +0200

    Add stop interval functionality via callback prop

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,18 +6,26 @@ import { useState } from "react";
 export function Layout() {
     const [message, setMessage] = useState('Layout works in state!');
     const [counter, setCounter] = useState(0);
+    const [intervalId, setIntervalId] = useState();
 
     const onClickHandler = () => {
-        setInterval(() => {
+        const id = setInterval(() => {
             setCounter(oldCount => oldCount + 1);
         }, 500);
+
+        setIntervalId(id);
+    }
+
+    const stopInterval = () => {
+        clearInterval(intervalId);
+        setIntervalId(null);
     }
 
     return (
         <>
             <Header />
             <button onClick={onClickHandler}>Start counter</button>
-            <Main message={message} counter={counter} />
+            <Main message={message} counter={counter} stopIntervalFn={stopInterval} />
             <Footer />
         </>
     )
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,15 @@
-export function Main({ message, counter }) {
+export function Main({ message, counter, stopIntervalFn }) {
+
+    const onClickHandler = () => {
+        stopIntervalFn();
+    }
+
     return (
         <div className="main-content">
             Main works!
             <br />{message}
             <br />Counter: {counter}
+            <br /><button onClick={onClickHandler}>Stop interval</button>
         </div>
     )
 }
