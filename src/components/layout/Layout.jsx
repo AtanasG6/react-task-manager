b17985ce704d8bde8922ea1ca9856ab9b79a8c09
AtanasG6 <gyulchev666@gmail.com>
2026-02-23 08:37:04 +0200

    Add Main component, update Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Footer } from "../footer/Footer";
 import { Header } from "../header/Header";
+import { Main } from "../main/Main";
+import { useState } from "react";
 
 export function Layout() {
+    const [message, setMessage] = useState('Layout works in state!');
+
     return (
         <>
             <Header />
-            Layout works!
+            <Main message={message} />
             <Footer />
         </>
     )
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,5 @@
+export function Main({ message }) {
+    return (
+        <div className="main-content">Main works! <br />{message}</div>
+    )
+}
