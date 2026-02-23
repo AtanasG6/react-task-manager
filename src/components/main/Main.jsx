183afddb48070b18e9aba78e83af92beccdecc27
AtanasG6 <gyulchev666@gmail.com>
2026-02-23 11:06:24 +0200

    Add react-router-dom, set up routing for users list and user detail

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,14 @@
 import { UsersList } from "../users/users-list/UsersList";
+import { User } from "../users/user/User";
+import { Route, Routes } from "react-router-dom";
 
 export function Main() {
     return (
         <div className="main-content">
-            <UsersList />
+            <Routes>
+                <Route path="/users-list" element={<UsersList />} />
+                <Route path="/user/:id" element={<User />} />
+            </Routes>
         </div>
     )
 }
---
+++ src/components/users/user/User.jsx
@@ -0,0 +1,9 @@
+import { UserCard } from "../user-card/UserCard";
+
+export function User() {
+  return (
+    <div className="user">
+        <UserCard />
+    </div>
+  )
+}
